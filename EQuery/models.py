a13593248d7e7a0e4131ea6d5498479fe4492956
hiakri <1020393360@qq.com>
2018-04-24 19:33:20 +0800

    some changes

--- EQuery/models.py
+++ EQuery/models.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8 -*-
 from __future__ import unicode_literals
-from django.db import models
+from EImport.models import models
 
 # Create your models here.
 class Exception(models.Model):
